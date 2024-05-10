eab8b8b1beae45a7ac2bbd5dbf520d0cebd46d06
liuyue <liuyue@ctjsoft.com>
2024-05-10 15:34:07 +0800

    L3完结

--- js/index.js
+++ js/index.js
@@ -1,9 +1,18 @@
 $(function() {
+  // 打开后台页
   $("#toExtendBg").click( e => {
     window.open(chrome.extension.getURL('background.html'))
   })
+  // 弹出后台页标题
   $("#getBgTitle").click( e => {
     var bg = chrome.extension.getBackgroundPage()
+    alert(bg.document.title)
+  })
+  // 设置后台页标题并弹出
+  $("#setBgTitle").click( e => {
+    var newTitle = prompt("设置新的后台也标题", "新标题")
+    var bg = chrome.extension.getBackgroundPage()
+    bg.document.title = newTitle
     alert(bg.document.title)
   })
 })
