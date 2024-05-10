3bd319c846ce1b097fa805a27863fa41b932d744
liuyue <liuyue@ctjsoft.com>
2024-05-10 17:37:15 +0800

    L5:更新，获取webUrl

--- js/background.js
+++ js/background.js
@@ -9,7 +9,8 @@ function backJs() {
 // POPUP调用百度自动查询内容
 function autoSeachValue() {
   chrome.tabs.getSelected(null, function(tab) {
-    if (tab.title.indexOf('百度一下') != -1 || tab.title.indexOf('百度搜索') != -1) {
+    var webUrl = window.location;
+    if (webUrl == 'https://baidu.com/') {
       chrome.tabs.executeScript(null, {code: 'javascript: var bdSeachId = document.querySelector("#kw"); bdSeachId.value="chrome插件"; var buttonId = document.querySelector("#su").click()'})
     } else {
       alert("百度页面点击查询")
